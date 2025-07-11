661e77b1b35ce266bc29fdd11daa2179c9fe52e1
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-07-11 21:49:42 +0330

    chore: set up project structure with Card component and CSS module connection

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  return (
+    <Card />
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Card.module.css'
+
+function Card(){
+    return (
+        <div className={styles.d}>for test</div>
+    )
+}
+
+export default Card
